bffff332713831c574b6c88465e1dd4a69b8abef
Masthan <masthantech9316@gmail.com>
2026-04-11 20:23:40 -0400

    1A-setup

---
+++ product-service/src/test/java/prodcut_service/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package prodcut_service.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
